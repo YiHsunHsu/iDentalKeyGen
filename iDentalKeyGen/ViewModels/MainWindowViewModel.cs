0ef072c364acc5f1aa8699c059436b3e26fc7749
EasonHsu <justshee@hotmail.com>
2017-11-19 22:05:23 +0800

    加入專案檔案。

---
+++ iDentalKeyGen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using iDentalKeyGen.Class;
+using iDentalKeyGen.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace iDentalKeyGen.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase.PropertyChangedBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        private string clinicCode;
+        public string ClinicCode
+        {
+            get { return clinicCode; }
+            set
+            {
+                clinicCode = value;
+                OnPropertyChanged("ClinicCode");
+                OnPropertyChanged("ClincCodeEncode");
+            }
+        }
+
+        public string ClincCodeEncode
+        {
+            get { return string.IsNullOrEmpty(ClinicCode) ? string.Empty : KeyGenerator.SHA384Encode(ClinicCode); }
+        }
+
+        private RelayCommand _copyKey;
+        public ICommand CopyKeyToClipboard
+        {
+            get
+            {
+                if (_copyKey == null)
+                {
+                    _copyKey = new RelayCommand(CopyKey, CanCopyKey);
+                }
+                return _copyKey;
+            }
+        }
+
+        private void CopyKey(object parameter)
+        {
+            Clipboard.SetText(ClincCodeEncode);
+        }
+
+        private bool CanCopyKey()
+        {
+            if (string.IsNullOrEmpty(ClincCodeEncode))
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ iDentalKeyGen/ViewModels/ViewModelBase/PropertyChangedBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace iDentalKeyGen.ViewModels.ViewModelBase
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
